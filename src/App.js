e41f2f569cd1ac7beda508f852a833383de67114
dadesign0709 <dadesign0709@gmail.com>
2023-10-01 19:56:04 +0900

    feat: 회원가입 초기 페이지 html, css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>회원 가입</h1>
-      <div>시작</div>
-      <div>이름</div>
-      <div>닉네임</div>
-      <div>이메일</div>
-      <div>비밀번호</div>
-      <div>비밀번호 확인</div>
+      <section>
+        <div class='sign-box'>
+          <form action=''>
+            <h1>회원가입</h1>
+            <hr/>
+            <div className='signin'>
+                <label for='name'>이름</label>
+                <input type='text' name='name' id='name' autoComplete='off'></input>
+            </div>
+            <div className='signin'>
+                <label for='name'>닉네임</label>
+                <input type='text' name='nickname' id='nickname' autoComplete='off'></input>
+            </div>
+            <div className='signin'>
+                <label for='name'>이메일</label>
+                <input type='text' name='emailadd' id='emailadd' autoComplete='off'></input>
+            </div>
+            <div className='signin'>
+                <label for='name'>비밀번호</label>
+                <input type='text' name='password' id='password' autoComplete='off'></input>
+            </div>
+            <div className='signin'>
+                <label for='name'>비밀번호 확인</label>
+                <input type='text' name='checkpsw' id='checkpsw' autoComplete='off'></input>
+            </div>
+            <div class="btn">
+              <button type='submit'>가입하기</button>
+            </div>
+          </form>
+          </div>
+      </section>
     </div>
   );
 }
